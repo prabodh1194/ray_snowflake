89d43e166be39f1ab9297c34efe49990c0f1066b
Prabodh Agarwal <prabodh1194@gmail.com>
2023-09-25 00:22:07 +0530

    add a write step

--- src/data/snowflake/datasource.py
+++ src/data/snowflake/datasource.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
 import logging
+from typing import Iterable
 
 from ray.data import Datasource
-from ray.data.datasource import Reader
+from ray.data._internal.execution.interfaces import TaskContext
+from ray.data.block import Block
+from ray.data.datasource import Reader, WriteResult
+from snowflake.connector import connect
+from snowflake.connector.pandas_tools import write_pandas
 
 from data.snowflake.reader import _SnowflakeDatasourceReader
 
@@ -20,3 +25,23 @@ class SnowflakeDatasource(Datasource):
             connection_args=self._connection_args,
             query=self._query
         )
+
+    def write(
+        self,
+        blocks: Iterable[Block],
+        ctx: TaskContext,
+        **write_args,
+    ) -> WriteResult:
+        connection = connect(**self._connection_args)
+        for block in blocks:
+            block.to_pandas().pipe(
+                lambda df: write_pandas(
+                    connection,
+                    df,
+                    database=write_args["database"],
+                    schema=write_args["schema"],
+                    table_name=write_args["table_name"],
+                    auto_create_table=write_args.get("auto_create_table"),
+                    create_temp_table=write_args.get("create_temp_table"),
+                )
+            )
--- src/driver.py
+++ src/driver.py
@@ -13,11 +13,10 @@ ray_data_logger = logging.getLogger("ray.data")
 
 ray.init(logging_level=logging.DEBUG)
 
+snowflake_datasource = SnowflakeDatasource(connection_args=settings.SNOWFLAKE_CONNECTION_PROPS, query="SELECT * FROM LINEITEM")
+
 rds = ray.data.read_datasource(
-    SnowflakeDatasource(
-        connection_args=settings.SNOWFLAKE_CONNECTION_PROPS,
-        query="SELECT * FROM LINEITEM"
-    ),
+    snowflake_datasource,
     parallelism=3
 )
 
@@ -29,7 +28,17 @@ def _comp(_ds: Table):
     )
 
 
-ds = rds.map_batches(_comp, batch_format="pyarrow").to_pandas()
+_ds = rds.map_batches(_comp, batch_format="pyarrow")
+
+_ds.write_datasource(
+    snowflake_datasource,
+    database=f"PBD_{settings.SNOWFLAKE_CONNECTION_PROPS['database']}",
+    schema=settings.SNOWFLAKE_CONNECTION_PROPS["schema"],
+    table_name="LINEITEM",
+    auto_create_table=True,
+)
+
+ds = _ds.to_pandas()
 
 ray_data_logger.info("pbd cols %s", ds.columns)
 ray_data_logger.info("pd size %s", ds.size)
